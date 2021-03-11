8013112c1154a2e55d2934033db3fa7346c8805e
Gabriel Zeitoum <51757174+Zeitoum@users.noreply.github.com>
2021-03-11 11:53:35 -0300

    Add files via upload

---
+++ Lab1.hs
@@ -0,0 +1,17 @@
+
+ --Exercicio 2:
+
+    duplicar x = 2*x
+
+ --Exercicio 3:
+
+    quadriplica x = 2*duplicar x
+
+ --Exercicio 4:
+
+    hipotenusa co ca = sqrt(co^2 + ca^2) 
+
+ --Exercicio 5:
+
+    distancia [x1,y1] [x2,y2]  =  sqrt((x2 - x1)^2 + (y2 - y1)^2)
+ 
